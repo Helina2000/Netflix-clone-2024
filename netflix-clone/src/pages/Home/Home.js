149acf02ab3e8bc8a7f9a7f941337c6b5cd03859
Helina2000 <helinaabajifar@gmail.com>
2024-01-20 14:23:14 +0300

    Header and Footer

---
+++ netflix-clone/src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../component/Header/Header';
+import Footer from '../../component/Footer/Footer';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
+
+
+
